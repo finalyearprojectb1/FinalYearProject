ef7e42df86478b67bac4fd853df678a5dd6e8009
Tejaswini G <tgirish@kirusa.in>
2020-06-03 11:25:45 +0530

    Randomising the grid functionality added

--- captcha_project/patternUnlock/views.py
+++ captcha_project/patternUnlock/views.py
@@ -1,27 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core import serializers
+
 import random
 import json
+import numpy as np
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
+## Compute adjacent elements for element at (i,j) position in matrix(2d array)
+def findingNeighbors(two_d_array, i, j,graph) :
+    rowLimit = len(two_d_array)-1
+    columnLimit = len(two_d_array[0])-1
+
+    for x in range(max(0,i-1),min(i+1,rowLimit)+1):
+        for y in range(max(0,j-1), min(j+1,columnLimit)+1):
+            if(x != i or y != j) :
+                graph.setdefault(two_d_array[i][j], []).append(two_d_array[x][y])
 
 def index(request):
-    graph1 = {
-                0 : [4,3,1],
-                1 : [0,2,3,4,5],
-                2 : [1,4,5],
-                3 : [0,1,4,6,7],
-                4 : [0,1,2,3,5,6,7,8],
-                5 : [1,2,4,7,8],
-                6 : [3,4,7],
-                7 : [3,4,5,6,7,8],
-                8 : [4,5,7]
-            }
+    row = random.randint(2,5)
+    column = random.randint(2,5)
+    array = np.array(range(row*column)).reshape((row, column))
+    graph = {}
+    
+    ## call the above function(findingNeighbors) for each matrix element
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            findingNeighbors(array,i,j,graph)
+  
     for i in range(1):
-        r = random.randint(0,8)
-        visited = dfs(graph1,r, [])
-        json_pattern=json.dumps(visited)
+        total_numbers = row*column
+        r = random.randint(0,(row*column)-1)
+        visited = dfs(graph,r, [])
+        json_pattern = json.dumps(visited, cls=NpEncoder)
+    return render(request,'patternUnlock/index.html',{"pattern":json_pattern,"row":row,"column":column,"total_nos":total_numbers})
 
-    return render(request,'patternUnlock/index.html',{"pattern":json_pattern})
 
+## DFS Algorithm to generate unique pattern
 def dfs(graph, node, visited):
     random.shuffle(graph[node])
     if node not in visited:
@@ -30,3 +55,9 @@ def dfs(graph, node, visited):
             dfs(graph,n, visited)
     k = random.randint(4,9)
     return visited[:k]
+
+
+# Homepage
+def homepage(request):
+    return render(request,'patternUnlock/index.html')
+
